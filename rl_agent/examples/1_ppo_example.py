777180bc72a2259501c15a2102bc6d995a0927cc
BarisYazici <barisyazici@sabanciuniv.edu>
2018-12-23 07:22:08 -0800

    agent creates the gym_roboy env but doesn't act on the simulation

--- rl_agent/examples/1_ppo_example.py
+++ rl_agent/examples/1_ppo_example.py
@@ -1,24 +1,33 @@
 import gym
-
+import time
+import gym_roboy
 from stable_baselines.common.policies import MlpPolicy
 from stable_baselines.common.vec_env import DummyVecEnv
-from stable_baselines import PPO2
+from stable_baselines import PPO1
 
 
 def our_env_constructor() -> gym.Env:
-    return gym.make("MountainCarContinuous-v0")
+    return gym.make("msj-control-v0")
 
+def main():
 
-# The algorithms require a vectorized environment to run
-env = DummyVecEnv([our_env_constructor])
+	# The algorithms require a vectorized environment to run
+	env = DummyVecEnv([our_env_constructor])
 
-agent = PPO2(MlpPolicy, env, verbose=1)
+	agent = PPO1(MlpPolicy, env, verbose=1)
 
-agent.learn(total_timesteps=10000)
+	agent.learn(total_timesteps=10000)
 
-obs = env.reset()
-for _ in range(1000):
-    # internal_state is only used with recurrent policies
-    action, internal_state = agent.predict(obs)
-    obs, reward, done, info = env.step(action)
-    env.render()
+	obs = env.reset()
+	for _ in range(100):
+	    # internal_state is only used with recurrent policies
+	    obs = env.reset()
+	    done = False
+	    while not done:
+		    action, _ = agent.predict(obs)
+		    obs, reward, done, info = env.step(action)
+		    print("step reward:", reward)
+		    time.sleep(0.01)
+	    print('reached goal')
+if __name__ == '__main__':
+	main()
