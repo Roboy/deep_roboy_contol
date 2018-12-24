525dcf91b9f06b9ce5d9255b7f687c7b51584a89
BarisYazici <barisyazici@sabanciuniv.edu>
2018-12-24 08:00:09 -0800

    new changes on ppo to suit the plots

--- rl_agent/examples/1_ppo_example.py
+++ rl_agent/examples/1_ppo_example.py
@@ -1,33 +1,79 @@
 import gym
-import time
 import gym_roboy
+
+import time
+
+
+
 from stable_baselines.common.policies import MlpPolicy
 from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines import PPO1
 
+#for plot
+import os
+from stable_baselines.bench import Monitor
+from stable_baselines.results_plotter import load_results, ts2xy
+import numpy as np
+import matplotlib.pyplot as plt
+
+best_mean_reward, n_steps = -np.inf, 0
 
 def our_env_constructor() -> gym.Env:
     return gym.make("msj-control-v0")
 
-def main():
 
-	# The algorithms require a vectorized environment to run
-	env = DummyVecEnv([our_env_constructor])
+def callback(_locals, _globals):
+  """
+  Callback called at each step (for DQN an others) or after n steps (see ACER or PPO2)
+  :param _locals: (dict)
+  :param _globals: (dict)
+  """
+  global n_steps, best_mean_reward
+  # Print stats every 1000 calls
+  if (n_steps + 1) % 1000 == 0:
+      # Evaluate policy performance
+      x, y = ts2xy(load_results(log_dir), 'timesteps')
+      if len(x) > 0:
+          mean_reward = np.mean(y[-100:])
+          print(x[-1], 'timesteps')
+          print("Best mean reward: {:.2f} - Last mean reward per episode: {:.2f}".format(best_mean_reward, mean_reward))
 
-	agent = PPO1(MlpPolicy, env, verbose=1)
+          # New best model, you could save the agent here
+          if mean_reward > best_mean_reward:
+              best_mean_reward = mean_reward
+              # Example for saving best model
+              print("Saving new best model")
+              _locals['self'].save(log_dir + 'best_model.pkl')
+  n_steps += 1
+  return True
 
-	agent.learn(total_timesteps=10000)
 
-	obs = env.reset()
-	for _ in range(100):
-	    # internal_state is only used with recurrent policies
-	    obs = env.reset()
-	    done = False
-	    while not done:
-		    action, _ = agent.predict(obs)
-		    obs, reward, done, info = env.step(action)
-		    print("step reward:", reward)
-		    time.sleep(0.01)
-	    print('reached goal')
-if __name__ == '__main__':
-	main()
+# Create log dir
+log_dir = "/tmp/gym/"
+os.makedirs(log_dir, exist_ok=True)
+
+
+
+env = gym.make('msj-control-v0')
+#for plotting
+env = Monitor(env, log_dir, allow_early_resets= True)
+# The algorithms require a vectorized environment to run
+#env = DummyVecEnv([our_env_constructor])
+env = DummyVecEnv([lambda:env])
+
+agent = PPO1(MlpPolicy, env, verbose=1)
+
+agent.learn(total_timesteps=1000, callback= callback)
+
+obs = env.reset()
+for _ in range(100):
+    # internal_state is only used with recurrent policies
+    obs = env.reset()
+    done = False
+    while not done:
+	    action, _ = agent.predict(obs)
+	    obs, reward, done, info = env.step(action)
+	    print("step reward:", reward)
+	    time.sleep(0.01)
+    print('reached goal')
+
